9143d66ae9805ba47f324d1f677388ac380a1342
曹思逸 <1102977704@qq.com>
2020-11-28 14:00:37 +0800

    feat:鸟笼配置，茶几配置

--- modinfo.lua
+++ modinfo.lua
@@ -1,116 +1,117 @@
-
 name = "阿乐冰箱"
-description = "调整冰箱腐烂速度,背包增加冰箱功能，添加了盐盒保鲜,骨灰盒保鲜，以及鸟笼无需喂养(暂不可配置)"
+description = "调整冰箱腐烂速度,背包增加冰箱功能，盐盒保鲜,骨灰盒保鲜，鸟笼无需喂养，茶几保鲜"
 author = "ale"
-version = "1.3.6"
+version = "1.3.7"
 
 icon = "modicon.tex"
 icon_atlas = "modicon.xml"
-
 
 api_version = 10
 
 dst_compatible = true
 
-
 all_clients_require_mod = true
-
 
 client_only_mod = false
 
-server_filter_tags = {icebox,backpack}
+server_filter_tags = {icebox, backpack}
 
-
-priority=-1
+priority = -1
 
 forumthread = ""
 
-configuration_options =
-{
-	{
-		name = "icebox_freeze",
-		label = "腐烂速度",
-		options =	{
-                        {description = "正常腐烂", data = "0.5"},
-                        {description = "缓慢腐烂", data = "0.3"},
-                        {description = "保鲜", data = "0"},
-						{description = "反鲜", data = "-5"},
-						
-					},
-
-		default = "0.5",
-	
-	},
-	{
+configuration_options = {
+    {
+        name = "icebox_freeze",
+        label = "开启后物品的腐烂速度",
+        options = {
+            {description = "正常腐烂", data = "0.5"},
+            {description = "缓慢腐烂", data = "0.3"},
+            {description = "保鲜", data = "0"},
+            {description = "反鲜", data = "-5"}
+        },
+        default = "0.5"
+    },
+    {
         name = "krampus_sack_ice",
         label = "小偷包保鲜",
-        options =
-        {
-			{description = "开启", data = true, hover = "小偷包保鲜,保鲜度同冰箱"}, 
-			{description = "关闭", data = false}, 
+        options = {
+            {description = "开启", data = true, hover = "开启保鲜模式"},
+            {description = "关闭", data = false}
         },
-        default = false,
+        default = false
     },
-	{
+    {
         name = "backpack_ice",
         label = "背包保鲜",
-        options =
-        {
-			{description = "开启", data = true, hover = "背包包保鲜,保鲜度同冰箱"}, 
-			{description = "关闭", data = false}, 
+        options = {
+            {description = "开启", data = true, hover = "开启保鲜模式"},
+            {description = "关闭", data = false}
         },
-        default = false,
+        default = false
     },
-	{
+    {
         name = "piggyback_ice",
         label = "小猪包保鲜",
-        options =
-        {
-			{description = "开启", data = true, hover = "小猪包保鲜,保鲜度同冰箱"}, 
-			{description = "关闭", data = false}, 
+        options = {
+            {description = "开启", data = true, hover = "开启保鲜模式"},
+            {description = "关闭", data = false}
         },
-        default = false,
+        default = false
     },
     {
         name = "saltlicker",
-		label = "盐盒",
-		options =	{
-                        {description = "正常腐烂", data = 0.25 },
-                        {description = "保鲜", data = 0 },
-						{description = "反鲜", data = -5 },
-						
-					},
+        label = "盐盒，单独设置保鲜",
+        options = {
+            {description = "正常腐烂", data = 0.25},
+            {description = "保鲜", data = 0},
+            {description = "反鲜", data = -5}
+        },
         default = -5
     },
     {
         name = "mushroom_frige",
-		label = "蘑菇灯保鲜",
-		options =	{
-                        {description = "正常腐烂", data = 0.25 },
-                        {description = "保鲜", data = 0 }
-						
-					},
+        label = "蘑菇灯保鲜，单独设置保鲜",
+        options = {
+            {description = "正常腐烂", data = 0.25},
+            {description = "保鲜", data = 0}
+        },
         default = 0
     },
     {
         name = "cage_frige",
-		label = "骨灰盒保鲜",
-		options =	{
-            {description = "开启", data = true, hover = "骨灰盒保鲜,保鲜度同冰箱"}, 
-			{description = "关闭", data = false}, 
-						
-					},
+        label = "骨灰盒保鲜",
+        options = {
+            {description = "开启", data = true, hover = "开启保鲜模式"},
+            {description = "关闭", data = false}
+        },
         default = true
     },
     {
         name = "cook_pot",
-		label = "锅上的食物不腐烂",
-		options =	{
-            {description = "开启", data = true, hover = "锅上的食物不腐烂"}, 
-			{description = "关闭", data = false}, 
-						
-					},
+        label = "锅上的食物不腐烂",
+        options = {
+            {description = "开启", data = true, hover = "锅上的食物不腐烂"},
+            {description = "关闭", data = false}
+        },
+        default = true
+    },
+    {
+        name = "bird_cage",
+        label = "设置鸟笼无需喂养",
+        options = {
+            {description = "开启", data = true, hover = "鸟笼无需喂养"},
+            {description = "关闭", data = false, hover = "关闭"}
+        },
+        default = true
+    },
+    {
+        name = "endtable",
+        label = "茶几上的花不坏",
+        options = {
+            {description = "开启", data = true, hover = "10000天不坏"},
+            {description = "关闭", data = false, hover = "默认设置"}
+        },
         default = true
     }
 }
-
--- modmain.lua
+++ modmain.lua
@@ -3,47 +3,79 @@ local backpack_ice = GetModConfigData("backpack_ice")
 local piggyback_ice = GetModConfigData("piggyback_ice")
 local cage_fridge = GetModConfigData("cage_frige")
 local cook_pot = GetModConfigData("cook_pot")
+local endtable = GetModConfigData("endtable")
+local bird_cage = GetModConfigData("bird_cage")
+
 TUNING.PERISH_FRIDGE_MULT = GetModConfigData("icebox_freeze")
 TUNING.PERISH_SALTBOX_MULT = GetModConfigData("saltlicker")
-
 TUNING.PERISH_MUSHROOM_LIGHT_MULT = GetModConfigData("mushroom_frige")
-if cage_fridge then 
-	AddPrefabPostInit("sisturn", function (inst)
-		inst:AddTag("fridge")
-		inst:AddTag("nocool")
-	end)
-end
-if krampus_sack_ice then
-		AddPrefabPostInit("krampus_sack", function (inst)
+
+if cage_fridge then
+	AddPrefabPostInit(
+		"sisturn",
+		function(inst)
 			inst:AddTag("fridge")
 			inst:AddTag("nocool")
-		end)
+		end
+	)
+end
+if krampus_sack_ice then
+	AddPrefabPostInit(
+		"krampus_sack",
+		function(inst)
+			inst:AddTag("fridge")
+			inst:AddTag("nocool")
+		end
+	)
 end
 
 if backpack_ice then
-		AddPrefabPostInit("backpack", function (inst)
+	AddPrefabPostInit(
+		"backpack",
+		function(inst)
 			inst:AddTag("fridge")
 			inst:AddTag("nocool")
-		end)
+		end
+	)
 end
 
 if piggyback_ice then
-		AddPrefabPostInit("piggyback", function (inst)
+	AddPrefabPostInit(
+		"piggyback",
+		function(inst)
 			inst:AddTag("fridge")
 			inst:AddTag("nocool")
-		end)
+		end
+	)
 end
-if cook_pot then 
-	
-	AddPrefabPostInit("portablecookpot", function (inst)
-		if inst.components.stewer then 
-			inst.components.stewer.onspoil = nil
+if cook_pot then
+	AddPrefabPostInit(
+		"portablecookpot",
+		function(inst)
+			if inst.components.stewer then
+				inst.components.stewer.onspoil = nil
+			end
 		end
-	end)
+	)
 
-	AddPrefabPostInit("cookpot", function (inst)
-		if inst.components.stewer then 
-			inst.components.stewer.onspoil = nil
+	AddPrefabPostInit(
+		"cookpot",
+		function(inst)
+			if inst.components.stewer then
+				inst.components.stewer.onspoil = nil
+			end
 		end
-	end)
+	)
+end
+
+if bird_cage then
+	TUNING.PERISH_CAGE_MULT = 0
+else
+	TUNING.PERISH_CAGE_MULT = 0.25
+end
+
+if endtable then
+	TUNING.ENDTABLE_FLOWER_WILTTIME = 30 * 16 * 10000
+else
+	TUNING.ENDTABLE_FLOWER_WILTTIME = 30 * 16 * 2.25
 end
